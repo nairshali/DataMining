554e526cd66e6c1a2b6081163d7b0a3d7e788da8
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-07-12 14:05:22 +0100

    Loop for 20 iterations, for each iteration random train / test sample
    Define loop for 20 iteration on wine dataset and for each iteration it will choose the random train / test sample with 90/10 respectively

--- R/Task_5_wine.R
+++ R/Task_5_wine.R
@@ -10,3 +10,13 @@ wine <- transform(wine, class = as.factor(class))
       sampleRate <- 0.10 
       sampleSize <- nrow(wine) * sampleRate 
       paste("sample size is: ", sampleSize) 
+
+   # 20 Trials - Loop for each type of method
+      for (t in 1:20){
+        #set.seed(1234) 
+        testSampleIdx <- sample(nrow(wine), size=sampleSize) #testSampleIdx 
+        testSet <- wine[testSampleIdx,] #testSet 
+        trainingSet <- wine[-testSampleIdx,] #trainingSet 
+        paste("Test set size<",nrow(testSet),">; Training set size<", nrow(trainingSet), ">", sep="") 
+        
+      }
