abb0dcc92bcb776994a63134a44baa2592a183e1
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-07-12 14:15:46 +0100

    Prediction using Resubstitution method

--- R/Task_5_wine.R
+++ R/Task_5_wine.R
@@ -10,6 +10,10 @@ wine <- transform(wine, class = as.factor(class))
       sampleRate <- 0.10 
       sampleSize <- nrow(wine) * sampleRate 
       paste("sample size is: ", sampleSize) 
+  
+  # initialize Variable 
+      resub_accuracy <- list()
+      hold_out_accuracy <- list()
 
    # 20 Trials - Loop for each type of method
       for (t in 1:20){
@@ -18,5 +22,17 @@ wine <- transform(wine, class = as.factor(class))
         testSet <- wine[testSampleIdx,] #testSet 
         trainingSet <- wine[-testSampleIdx,] #trainingSet 
         paste("Test set size<",nrow(testSet),">; Training set size<", nrow(trainingSet), ">", sep="") 
+        
+        
+        wine.dt <- rpart(class ~., data=trainingSet, method="class")  #wine.dt   
+        
+        # Resubstitution method: acc/error on the training set 
+        prediction <- predict(wine.dt, newdata=trainingSet, type="class") 
+        cM <- table(trainingSet$class, prediction) #print(cM) 
+        acc <- sum(diag(cM))/sum(cM) 
+        resub_accuracy[t] = acc
+        rErr <- 1.0 - acc 
+        print(paste("resub method: accuracy = ", round(acc*100,1), "% and resubstitution error = ", round(rErr*100,1), "%", sep="")) 
+        
         
       }
