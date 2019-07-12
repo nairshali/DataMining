5ccb38ca7051d9b511c0d95d7c6e78bd362e16d9
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-07-12 13:59:56 +0100

    Read wine data sets and define variable for 10% test sample
    Divide dataset in 90 -10 ratio, 90% Train and 10% Test

--- R/Task_5_wine.R
+++ R/Task_5_wine.R
@@ -1,1 +1,12 @@
+library(rpart) 
 
+# Load data
+wine <-read.table(file.choose(),header=T,sep=",") 
+wine <- transform(wine, class = as.factor(class))
+
+## Resubstitution / Hold- Out Method
+
+  # Data sampling
+      sampleRate <- 0.10 
+      sampleSize <- nrow(wine) * sampleRate 
+      paste("sample size is: ", sampleSize) 
