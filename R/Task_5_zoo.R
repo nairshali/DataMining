392bfe388a40b3355de5bc70a4c2bc888b2d2927
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-07-17 23:44:49 +0100

    read data file comments

--- R/Task_5_zoo.R
+++ R/Task_5_zoo.R
@@ -1,7 +1,7 @@
 library(rpart) 
 
 # Load data
-# Load data
+# read data file and store in zoo variable
 zoo <-read.csv(file.choose(),header=T,sep=",") 
 zoo$animal.name <- NULL
 zoo <- transform(zoo, type = as.factor(type))
