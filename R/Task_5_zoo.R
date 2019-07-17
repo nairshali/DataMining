e8aab461af5e0363f5a97836653e2cf9a673207b
Shalini Nair <47002450+nairshali@users.noreply.github.com>
2019-07-18 00:07:03 +0100

    Initialize Variables

--- R/Task_5_zoo.R
+++ R/Task_5_zoo.R
@@ -11,6 +11,9 @@ sampleRate <- 0.10
 sampleSize <- nrow(zoo) * sampleRate 
 paste("sample size is: ", sampleSize) 
 
+resub_accuracy <- list()
+hold_out_accuracy <- list()
+
 # Loop for 20 iteration and identify training / test sample data 90/10 ratio for each iterations 
 for (t in 1:20){
         #set.seed(1234) 
